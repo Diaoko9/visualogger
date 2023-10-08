d5a44b7c1e686e8f0952f4f1d2fbeb41faf94eba
Diaoko <ahmad.p.92@gmail.com>
2023-10-08 23:01:43 +0330

    1

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open Camera")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+if not ret:
+    break
+    print("Cant recieve frame...")
+
+
+
+
+gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+cv.imshow('frame',gray)
+
+if cv.waitkey(1) == ord('q'):
+    break
+
+cap.release()
+cv.destroyAllwindows()
