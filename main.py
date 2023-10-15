1b59e2e75e7f356488915e8b8d7735c8019cec9a
Diaoko <ahmad.p.92@gmail.com>
2023-10-15 12:43:11 +0330

    text detection added

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import numpy as np
 import cv2 as cv
-<<<<<<< HEAD
+
 from pytesseract import pytesseract
-=======
->>>>>>> parent of bba72f8 (1)
+
 
 #pipeline = 'device=/dev/video0 ! camera-id=0 ! video/x-h264, format=YUY2 ! videoconvert ! format=BGR ! appsink drop=1'
 #pipeline = "device=/dev/video0 ! camera-id=0 ! autovideosink"
@@ -14,16 +13,35 @@ if not cap.isOpened():
     print("Cannot open Camera")
     exit()
 
+counter = 0
+
 while True:
  ret, frame = cap.read()
+ counter+=1
 
- if not ret:
-    print("Cant recieve frame...")
-    break
+ if ((counter%20)==0):
+        
+    imgH, imgW,_ = frame.shape
+        
+    x1,y1,w1,h1 = 0,0,imgH,imgW
+        
+    imgchar = pytesseract.image_to_string(frame)
+        
+    imgboxes = pytesseract.image_to_boxes(frame)
+        
+    for boxes in imgboxes.splitlines():
+        boxes = boxes.split(' ')
+        x,y,w,h = int(boxes[1]), int(boxes[2]), int(boxes[3]), int(boxes[4])
+        cv.rectangle(frame, (x, imgH-y), (w,imgH-h), (0,0,255),3)
+            
+        cv.putText(frame, imgchar, (x1 + int(w1/50), y1 + int(h1/50)), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2)
+        
+        font = cv.FONT_HERSHEY_SIMPLEX
 
+ #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+ 
 
- gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
  cv.imshow('frame',frame)
 
@@ -32,7 +50,3 @@ while True:
 
 cap.release()
 cv.destroyAllwindows()
-
-
-def tesseract():
-    
